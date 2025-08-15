ec9f0cfaeb6895aab255c9acec2cd0fad2cccc96
Ayza <justforgame.1380@gmail.com>
2025-08-15 06:05:19 +0330

    chore: static export for Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Silence warnings
-  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+  output: 'export', // static export
 };
-
 export default nextConfig;
